9b7918c5b2aa49dd0ff733670820c7507ba62e44
carmenvchirica <carmenv.chirica@gmail.com>
2024-10-08 13:07:17 +0300

    Todo app before standalone

---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrl: './welcome.component.css'
+})
+export class WelcomeComponent implements OnInit {
+
+  name = '';
+  message = 'Welcome to our amazing page!';
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.name = this.route.snapshot.params['name'];
+    console.log( this.name);
+  }
+}
