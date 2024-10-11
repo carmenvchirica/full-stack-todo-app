d68bd14367c636405fd7412a9f89d13390e687e4
carmenvchirica <carmenv.chirica@gmail.com>
2024-10-11 14:53:58 +0300

    Removed unused imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from './footer/footer.component';
-import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
